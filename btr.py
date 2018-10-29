30e4b6d8557422f3f495ec992525069c86d6cbf3
Ubuntu 16.04 <mjr2563@rit.edu>
2018-10-29 10:52:16 -0700

    Templated files

---
+++ btr.py
@@ -0,0 +1,24 @@
+# File: btr.py
+# Description:
+# Usage: 
+
+# default paths for browser data for each browser
+
+
+def getArgs():
+	return
+
+def getConfig(filename, username=None, browserlist):
+	return
+
+def getCache(filename, browser):
+	return
+
+def getHistory(filename, browser):
+	return
+
+def getCookies(filename, browser):
+	return
+
+def printData(data):
+	return
