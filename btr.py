cc7bb899f5df7e8e021ea13a6ac1f3a3ce37c8b5
Ubuntu 16.04 <mjr2563@rit.edu>
2018-11-15 08:16:24 -0800

    start/end time functionality, documentation

--- btr.py
+++ btr.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 
 # File: btr.py
-# Description:
-# Usage: 
+# Description: Analyzes timestamps on browser artifacts to potentially detect and
+#              recover deleted browsing history. Detected records are outputted in
+#              CSV format. Also, provides the option to dump records found.
+ 
 
 import argparse
 import yaml
 import sqlite3
+import csv
+from datetime import datetime
 from platform import system
-from sys import exit
+from sys import exit,stdout,stderr
 from os import listdir, path
 from getpass import getuser
 
@@ -41,7 +45,7 @@ paths = {
 	}
 }
 acceptedBrowsers = ['chrome','firefox']
-dumpOptions = ['all','history','cookies','cache']
+dumpOptions = ['history','cookies','cache']
 
 
 ### Functions ###
@@ -54,14 +58,18 @@ def getArgs():
 		help="Select a browser to examine", required=True)
 	parser.add_argument('-d','--dump',choices=dumpOptions,dest='dump',
 		help="Dump data in CSV format and quit")
+	parser.add_argument('-o','--outfile',dest='out',
+		help="File to write results")
 	parser.add_argument('-u','--user',dest='user',
 		help="Specifiy a user directory to search, default is current user")
 	parser.add_argument('-c','--config',dest='config',
 		help="Specify YAML configuration file with paths to files")
-	parser.add_argument('-s','--start',dest='start',
-		help="Starting time for analysis")
+	parser.add_argument('-s','--start',dest='start',default=0,
+		help="Starting time for analysis (epoch/NTFS file time)")
 	parser.add_argument('-e','--end',dest='end',
-		help="Ending time for analysis")
+		help="Ending time for analysis (epoch/NTFS file time)")
+	parser.add_argument('-w','--window',dest='window',default=5000000,
+		help="Time window to check for discrepancies between artifacts (e.g. cookies) with history (microseconds). DEFAULT: 5000000 (5s)")
 	return parser.parse_args()
 
 
@@ -139,28 +147,31 @@ def setConfig(systemType, configFile, browser, user):
 def getCache(filename, browser):
 	return
 
-#TODO verify file exists in case a user gives wrong path or if not installed
+
+# returns a list of visited urls from the given browser
 def getHistory(filename, browser):
-	if not path.isfile(filename): raise Exception(f"File:{filename} not found.\nIf you know the location of the history file for {browser}, add it to the configuration file.")
+	if not path.isfile(filename): 
+		raise Exception("File:{0} not found.\nIf you know the location of the history file for {1}, add it to the configuration file.".format(filename,browser))
 	connection = sqlite3.connect(filename)
 	connection.text_factory = str
 	cur = connection.cursor()
 	History = list()
 	
 	if(browser == "firefox"):
-		for row in (cur.execute('SELECT url, title, last_visit_date, guid FROM moz_places')):
+		for row in (cur.execute('SELECT url, title, last_visit_date FROM moz_places')):
 			History.append(list(row))
 	elif(browser == "chrome"):
-		for row in (cur.execute('SELECT url, title, visit_count, last_visit_time FROM urls')):
+		for row in (cur.execute('SELECT url, title, last_visit_time FROM urls')):
 			History.append(list(row))
 
 		# return list that contains only entries with populated timestamps
 	return list(hist for hist in History if hist[2])
 
 
-#TODO verify file exists in case a user gives wrong path or if not installed
+# returns a list of cookes from the given browser
 def getCookies(filename, browser):
-	if not path.isfile(filename): raise Exception(f"File:{filename} not found.\nIf you know the location of the cookie file for {browser}, add it to the configuration file.")
+	if not path.isfile(filename): 
+		raise Exception("File:{0} not found.\nIf you know the location of the cookie file for {1}, add it to the configuration file.".format(filename,browser))
 	connection = sqlite3.connect(filename)
 	connection.text_factory = str
 	cur = connection.cursor()
@@ -182,18 +193,40 @@ def printData(data):
 
 
 # compare timestamps from different sources to find any discrepancies 
-def analyzeTimestamps(history, cookies):
+def analyzeTimestamps(history, cookies, start, end, win):
 	discrepancies = []
-	win = 3600000	# time window in microseconds
 
 	# loop through cookies and compare each entry to the timestamps in history
 	for c in cookies:
 		# if creation date doesn't match a timestamp (with a given window)
 		# append the cookie to the list
 		matchHist = [h for h in history if c[2]-win <= h[2] <= c[2]+win]
+
+		# remove history outside the start-end times
+#		if (start or end):
+#			if (start):
+		if (end):	# both start and end set
+			matchHist = [h for h in matchHist if start <= h[2] <= end]
+		else: # only start set
+			matchHist = [h for h in matchHist if start <= h[2]]
+#			else: # only end set
+#				matchHist = [h for h in matchHist if h[2] <= end]
+				
 		if len(matchHist) == 0:
 			discrepancies.append(c)
 	return discrepancies
+
+# outputs given list to a file or stdout in csv format
+def printList (l, f):
+	try:
+		f = f and open(f,'w') or stdout
+		writer = csv.writer(f, lineterminator='\n')
+		writer.writerows(l)
+		if f is not stdout:
+			f.close()
+	except Exception as e:
+		print (e)
+
 
 if __name__ == '__main__':
 	args = getArgs()
@@ -203,12 +236,40 @@ if __name__ == '__main__':
 	setConfig(system(), args.config, args.browser, user)
 	history = getHistory(paths[system()][args.browser]['history'][0], args.browser)
 	cookies = getCookies(paths[system()][args.browser]['cookies'][0], args.browser)
-	buf = analyzeTimestamps(history, cookies)
-	print ('history',len(history))
-	print ('cookies',len(cookies))
-	print ('buf',len(buf))
-	if(args.dump == "history"):
-		print(history)
-	elif(args.dump == "cookies"):
-		print(cookies)
-	#print(yaml.dump(paths))
+
+	# dump options
+	if (args.dump == "history"):
+		history.insert(0,['url', 'title', 'last_visit_time'])
+		printList(history, args.out)
+		exit()
+	if (args.dump == "cookies"):
+		cookies.insert(0,['host', 'name', 'creationTime', 'expiry', 'lastAccessed'])
+		printList(cookies, args.out)
+		exit()
+
+	print ('History records:',len(history),file=stderr)
+	print ('Cookie records:',len(cookies),file=stderr)
+
+	# convert windows timestamps to datetime
+	if system() == "Windows":
+		start = datetime(1601,1,1) + timedelta(microseconds=(int(args.start,16)/10))
+		if not args.end:
+			end = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+		else:
+			end = datetime(1601,1,1) + timedelta(microseconds=(int(args.end,16)/10))
+	else: # convert Linux timestamps
+		start = datetime.fromtimestamp(args.start / 1e3)
+		if not args.end:
+			end = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+		else:
+			end = datetime.fromtimestamp(args.start / 1e3)
+	print ("Searching for discrepancies from",start,"to",end,file=stderr)
+
+	# analyze timestamps
+	buf = analyzeTimestamps(history, cookies, args.start, args.end, args.window)
+	print ('Discrepancies found:',len(buf),file=stderr)
+	
+	buf.insert(0,['host', 'name', 'creationTime', 'expiry', 'lastAccessed'])
+	printList(buf, args.out)
+
+
